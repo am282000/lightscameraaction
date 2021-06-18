6667f87570ff261e53dcc9075b8c885394b07b1c
am282000 <madaansahaab777@gmail.com>
2021-06-19 02:33:08 +0530

    TrendingPage Updated

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Movies = () => {
+	const [page, setPage] = useState(1);
+
+	const fetchMovies = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+		);
+	};
+
 	return (
 		<>
-			<span className="pagetitle">Movies</span>
+			<span className="pagetitle">Movies 1: 04 :00</span>
 		</>
 	);
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-	return <span className="header"> Lights 🔅 Camera 📸 Action ⚔️</span>;
+	return (
+		<span onClick={() => window.scroll(0, 0)} className="header">
+			Lights 🔅 Camera 📸 Action ⚔️
+		</span>
+	);
 };
 
 export default Header;
